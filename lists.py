74f63b1559d668b20261540fe6c02f793a63bca9
xlmc (Ryan) <146887380+HeavenlyHellish@users.noreply.github.com>
2025-08-28 18:32:10 +0000

    samalama

--- lists.py
+++ lists.py
@@ -1,3 +1,11 @@
-import pandas as biblicallyaccuratepotato
-import disney_movies_total_gross.csv
+import pandas as samalamadumalamayouassuminimahumanwhatigottadotogetitthroughtoyouimsuperhumaninnovativeandimmadeofrubbersothatanythingyousayisrichochetingoffmeanditllgluetoyou
 
+data = samalamadumalamayouassuminimahumanwhatigottadotogetitthroughtoyouimsuperhumaninnovativeandimmadeofrubbersothatanythingyousayisrichochetingoffmeanditllgluetoyou.read_csv('data.csv', header = 0, sep=',')
+print(data)
+
+cleanData = data.dropna(axis=0, inplace=False)
+
+print(data.iloc[8])
+print(cleanData.iloc[8])
+
+print(cleanData.describe())
